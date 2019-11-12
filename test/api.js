ac7efa0b86ccf11ce67a294db7b65e3aacc9c0b0
yelo <zhihuzeye@gmail.com>
2019-11-12 21:26:33 +0800

    refactor: jsbox-webpack-plugin
    - builtin zip func via yazl without gulp
    - builtin copy func instead of extra copy-webpack-plugin
    - builtin `main.js` generator

--- test/api.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava'
-
-test.skip('jsbox api', (t) => {
-  // TODO
-  // const jsbox = require('../lib/jsbox')
-  // jsbox.sync('192.168.2.113', path.resolve(__dirname, '../../../dist/每日签到'))
-  t.pass()
-})
